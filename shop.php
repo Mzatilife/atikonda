9bad4a4e067361a370a8fb21994e38bcfe61064e
Mahala Mzati <mahalamkwepu@gmail.com>
2022-10-26 16:10:47 -0700

    added an image

--- shop.php
+++ shop.php
@@ -53,7 +53,7 @@ $profile = new ProfileContr;
     <!-- ##### Breadcrumb Area Start ##### -->
     <div class="breadcrumb-area">
         <!-- Top Breadcrumb Area -->
-        <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center" style="background-image: url(img/bg-img/24.jpg);">
+        <div class="top-breadcrumb-area bg-img bg-overlay d-flex align-items-center justify-content-center" style="background-image: url(./assets/img/flowers.jpg);">
             <h2>Ati's Planters</h2>
         </div>
 
